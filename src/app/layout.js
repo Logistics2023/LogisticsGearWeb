53be04b0cb66954f8ac44381f5370d186f27c14f
Logistics2023 <131830463+Logistics2023@users.noreply.github.com>
2023-07-28 05:50:05 -0400

    Update layout.js
    DC

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,9 +13,9 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/logo-dark.svg" />
         <link rel='manifest' href='/manifest.json' />
         <link rel='apple-touch-icon' href='/logo-dark.svg' />
-        <meta name="theme-color" content="#2A52BE" />
-        <meta name="msapplication-navbutton-color" content="#0064FA" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="#0064FA" />
+        <meta name="theme-color" content="#030712" />
+        <meta name="msapplication-navbutton-color" content="#030712" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="##030712" />
         <meta name="description" content="Sistema de Administracion de Clientes y Formularios" />
         <meta name="keywords" content="Logistics Gear" />
         <meta name="author" content="Logistics Gear" />
